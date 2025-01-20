0da7048c358e201d00840791cbae74ec9bb7df70
MarcusL-O <marcus.lisolivebring@iths.se>
2025-01-20 11:48:07 +0100

    connection done

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
+
+
+app.get("/", (reg, res) =>{
+    res.send("Welcome to my api");
+});
+
+app.post("/blogpost", (reg, res) => {
+    res.send("This is a postrequest");
+});
 
 
 
-app.listen(PORT, () => {
-    console.log("Listend to port 3000");
+mongoose.
+connect("mongodb+srv://Marcuslo00:SKibk0033@marcusapi.4eeoo.mongodb.net/Products-API?retryWrites=true&w=majority&appName=MarcusAPI")
+.then(() =>{
+    console.log("Connected to mongo");
+    
+    app.listen(PORT, () => {
+        console.log("Listend to port 3000");
+    });
 });
